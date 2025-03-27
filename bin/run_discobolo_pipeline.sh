6126a8b1164beaba0dd6f762decfbdada015f3e7
Jotaene <nahuelseoane@gmail.com>
2025-03-27 01:02:58 -0300

    solving problems with pipeline in cron

--- bin/run_discobolo_pipeline.sh
+++ bin/run_discobolo_pipeline.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+PROJECT_DIR="/home/jotaene/proyectos/automating-discobolo"
+
+# Move to project root directory
+cd "$PROJECT_DIR"
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Fix import errors
+export PYTHONPATH=$PROJECT_DIR
+
+LOG="$PROJECT_DIR/logs/automation_pipeline.log"
+
+# DEBUG: Log environment at cron time
+env > ./logs/env_from_cron.log
+
 echo "Reached discobolo execution point" >> ./logs/automation_pipeline.log
 # Create temp lock/trace file
 touch /tmp/roadmap_ran.txt
-
-# Set environment
-./bin/set_env.sh
 
 echo "[$(date)] Python used:" >> ./logs/automation_pipeline.log
 which python >> ./logs/automation_pipeline.log
@@ -13,7 +26,7 @@ which python >> ./logs/automation_pipeline.log
 echo "[$(date)] Script started" >> ./logs/automation_pipeline.log
 
 # Run automation pipeline
-discobolo run >> ./logs/automation_pipeline.log 2>&1
+bash ./bin/automation_pipeline.sh >> ./logs/automation_pipeline.log 2>&1
 
 # Log end timestamp
 echo "[$(date)] Script finished" >> ./logs/automation_pipeline.log
